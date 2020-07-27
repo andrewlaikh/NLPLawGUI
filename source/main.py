3cbff76bf3828c34e5e7847cd6740b0205690ad3
andrewlaikh <andrewlaikh1@gmail.com>
2020-07-27 12:17:58 +0800

    Test file to play with layout

--- source/main.py
+++ source/main.py
@@ -2,11 +2,15 @@ import streamlit as st
 import streamlit.components.v1 as components
 from graph import Graph
 
-st.title("EU Legal documents visualised")
-st.markdown("This application helps to **visualise EU documents** and the **relevant provisions cited   **.")
+from source.variables import *
 
 graph = Graph()
-components.html(graph.defaultGraph, width=1000, height=550)
 
-if st.button("Generate Visualisation"):
-    graph.createGraph()
+st.title("EU Legal documents visualised")
+st.markdown("This application helps to **visualise EU documents** and the **relevant provisions cited   **.")
+option = st.selectbox('Select one piece of lex to visualise: ', ('', 'Example 1', 'Example 2', 'Example 3'))
+if option == 'Example 1':
+    graph.defaultGraph = example1
+# if st.button("Generate Visualisation"):
+#     graph.createGraph()
+components.html(graph.defaultGraph, width=1000, height=550)
--- source/variables.py
+++ source/variables.py
@@ -82,3 +82,112 @@ defaultGraph = """<html>
 </script>
 </body>
 </html>"""
+
+
+example1 = """
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/vis/4.16.1/vis.css" type="text/css" />
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vis/4.16.1/vis-network.min.js"> </script>
+
+<!-- <link rel="stylesheet" href="../node_modules/vis/dist/vis.min.css" type="text/css" />
+<script type="text/javascript" src="../node_modules/vis/dist/vis.js"> </script>-->
+
+<style type="text/css">
+
+        #mynetwork {
+            width: 1000px;
+            height: 500px;
+            background-color: #ffffff;
+            border: 1px solid lightgray;
+            position: relative;
+            float: left;
+        }
+
+        
+
+        
+
+        
+</style>
+
+</head>
+
+<body>
+<div id = "mynetwork"></div>
+
+
+<script type="text/javascript">
+
+    // initialize global variables.
+    var edges;
+    var nodes;
+    var network; 
+    var container;
+    var options, data;
+
+    
+    // This method is responsible for drawing the graph, returns the drawn network
+    function drawGraph() {
+        var container = document.getElementById('mynetwork');
+        
+        
+
+        // parsing and collecting nodes and edges from the python
+        nodes = new vis.DataSet([{"color": "blue", "group": 1, "id": "regulation 1913/2005", "label": "regulation 1913/2005", "shape": "dot", "size": 25, "title": "regulation 1913/2005 Neighbors:\u003cbr\u003eregulation 2771/75\u003cbr\u003eregulation 2529/2001\u003cbr\u003eregulation 806/2003\u003cbr\u003eregulation 2759/75\u003cbr\u003eregulation 1365/2000\u003cbr\u003eregulation 1255/1999\u003cbr\u003eregulation 1782/2003\u003cbr\u003eregulation 186/2004\u003cbr\u003eregulation 1254/1999\u003cbr\u003eregulation 2777/75\u003cbr\u003eregulation 1913/2005"}, {"color": "yellow", "group": 2, "id": "regulation 2759/75", "label": "regulation 2759/75", "shape": "dot", "size": 10, "title": "regulation 2759/75 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 2, "id": "regulation 2771/75", "label": "regulation 2771/75", "shape": "dot", "size": 10, "title": "regulation 2771/75 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 2, "id": "regulation 2777/75", "label": "regulation 2777/75", "shape": "dot", "size": 10, "title": "regulation 2777/75 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 2, "id": "regulation 1254/1999", "label": "regulation 1254/1999", "shape": "dot", "size": 10, "title": "regulation 1254/1999 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 2, "id": "regulation 1255/1999", "label": "regulation 1255/1999", "shape": "dot", "size": 10, "title": "regulation 1255/1999 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 2, "id": "regulation 2529/2001", "label": "regulation 2529/2001", "shape": "dot", "size": 10, "title": "regulation 2529/2001 Neighbors:\u003cbr\u003eregulation 467/2001\u003cbr\u003eregulation 215/2003\u003cbr\u003eregulation 881/2002\u003cbr\u003eregulation 145/2003"}, {"color": "yellow", "group": 2, "id": "regulation 1365/2000", "label": "regulation 1365/2000", "shape": "dot", "size": 10, "title": "regulation 1365/2000 Neighbors:\u003cbr\u003edirective 2002/12/ec\u003cbr\u003edecision 1999/468/ec\u003cbr\u003edirective 90/619/eec\u003cbr\u003edirective 2000/64/ec\u003cbr\u003edirective 85/611/eec\u003cbr\u003eregulation 1999/468\u003cbr\u003edirective 73/239/eec\u003cbr\u003edirective 93/22/eec\u003cbr\u003eregulation 92/96\u003cbr\u003edirective 91/675/eec\u003cbr\u003edirective 2001/65/ec\u003cbr\u003edirective 84/253/eec\u003cbr\u003edirective 1999/468/ec\u003cbr\u003edirective 79/267/eec\u003cbr\u003edirective 95/26/eec\u003cbr\u003edirective 91/674/eec\u003cbr\u003eregulation 93/22\u003cbr\u003edirective 2001/108/ec\u003cbr\u003eregulation 79/267\u003cbr\u003edirective 2000/12/ec\u003cbr\u003edirective 92/96/eec\u003cbr\u003edirective 2000/28/ec\u003cbr\u003edirective 2001/34/ec\u003cbr\u003edirective 83/349/eec\u003cbr\u003eregulation 90/619\u003cbr\u003edirective 78/660/eec\u003cbr\u003edirective 2002/83/ec\u003cbr\u003edirective 2002/13/ec"}, {"color": "yellow", "group": 2, "id": "regulation 806/2003", "label": "regulation 806/2003", "shape": "dot", "size": 10, "title": "regulation 806/2003 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 2, "id": "regulation 1782/2003", "label": "regulation 1782/2003", "shape": "dot", "size": 10, "title": "regulation 1782/2003 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 2, "id": "regulation 186/2004", "label": "regulation 186/2004", "shape": "dot", "size": 10, "title": "regulation 186/2004 Neighbors:\u003cbr\u003eregulation 228/2004\u003cbr\u003eregulation 1982/2005\u003cbr\u003eregulation 537/2004\u003cbr\u003eregulation 565/2002"}, {"color": "yellow", "group": 3, "id": "regulation 215/2003", "label": "regulation 215/2003", "shape": "dot", "size": 10, "title": "regulation 215/2003 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 881/2002", "label": "regulation 881/2002", "shape": "dot", "size": 10, "title": "regulation 881/2002 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 467/2001", "label": "regulation 467/2001", "shape": "dot", "size": 10, "title": "regulation 467/2001 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 145/2003", "label": "regulation 145/2003", "shape": "dot", "size": 10, "title": "regulation 145/2003 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 90/619", "label": "regulation 90/619", "shape": "dot", "size": 10, "title": "regulation 90/619 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 79/267", "label": "regulation 79/267", "shape": "dot", "size": 10, "title": "regulation 79/267 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 92/96", "label": "regulation 92/96", "shape": "dot", "size": 10, "title": "regulation 92/96 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 93/22", "label": "regulation 93/22", "shape": "dot", "size": 10, "title": "regulation 93/22 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 1999/468", "label": "regulation 1999/468", "shape": "dot", "size": 10, "title": "regulation 1999/468 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 2002/83/ec", "label": "directive 2002/83/ec", "shape": "dot", "size": 10, "title": "directive 2002/83/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 79/267/eec", "label": "directive 79/267/eec", "shape": "dot", "size": 10, "title": "directive 79/267/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 90/619/eec", "label": "directive 90/619/eec", "shape": "dot", "size": 10, "title": "directive 90/619/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 92/96/eec", "label": "directive 92/96/eec", "shape": "dot", "size": 10, "title": "directive 92/96/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 91/674/eec", "label": "directive 91/674/eec", "shape": "dot", "size": 10, "title": "directive 91/674/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 73/239/eec", "label": "directive 73/239/eec", "shape": "dot", "size": 10, "title": "directive 73/239/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 93/22/eec", "label": "directive 93/22/eec", "shape": "dot", "size": 10, "title": "directive 93/22/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 91/675/eec", "label": "directive 91/675/eec", "shape": "dot", "size": 10, "title": "directive 91/675/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 83/349/eec", "label": "directive 83/349/eec", "shape": "dot", "size": 10, "title": "directive 83/349/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 2001/34/ec", "label": "directive 2001/34/ec", "shape": "dot", "size": 10, "title": "directive 2001/34/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 84/253/eec", "label": "directive 84/253/eec", "shape": "dot", "size": 10, "title": "directive 84/253/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 78/660/eec", "label": "directive 78/660/eec", "shape": "dot", "size": 10, "title": "directive 78/660/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 85/611/eec", "label": "directive 85/611/eec", "shape": "dot", "size": 10, "title": "directive 85/611/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 2000/12/ec", "label": "directive 2000/12/ec", "shape": "dot", "size": 10, "title": "directive 2000/12/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 2002/12/ec", "label": "directive 2002/12/ec", "shape": "dot", "size": 10, "title": "directive 2002/12/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 2000/64/ec", "label": "directive 2000/64/ec", "shape": "dot", "size": 10, "title": "directive 2000/64/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 2002/13/ec", "label": "directive 2002/13/ec", "shape": "dot", "size": 10, "title": "directive 2002/13/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 2001/65/ec", "label": "directive 2001/65/ec", "shape": "dot", "size": 10, "title": "directive 2001/65/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 2001/108/ec", "label": "directive 2001/108/ec", "shape": "dot", "size": 10, "title": "directive 2001/108/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 2000/28/ec", "label": "directive 2000/28/ec", "shape": "dot", "size": 10, "title": "directive 2000/28/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 95/26/eec", "label": "directive 95/26/eec", "shape": "dot", "size": 10, "title": "directive 95/26/eec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "directive 1999/468/ec", "label": "directive 1999/468/ec", "shape": "dot", "size": 10, "title": "directive 1999/468/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "decision 1999/468/ec", "label": "decision 1999/468/ec", "shape": "dot", "size": 10, "title": "decision 1999/468/ec Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 1982/2005", "label": "regulation 1982/2005", "shape": "dot", "size": 10, "title": "regulation 1982/2005 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 565/2002", "label": "regulation 565/2002", "shape": "dot", "size": 10, "title": "regulation 565/2002 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 228/2004", "label": "regulation 228/2004", "shape": "dot", "size": 10, "title": "regulation 228/2004 Neighbors:\u003cbr\u003e"}, {"color": "yellow", "group": 3, "id": "regulation 537/2004", "label": "regulation 537/2004", "shape": "dot", "size": 10, "title": "regulation 537/2004 Neighbors:\u003cbr\u003e"}]);
+        edges = new vis.DataSet([{"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 1913/2005", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 2759/75", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 2771/75", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 2777/75", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 1254/1999", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 1255/1999", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 2529/2001", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 1365/2000", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 806/2003", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 1782/2003", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1913/2005", "to": "regulation 186/2004", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 2529/2001", "to": "regulation 215/2003", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 2529/2001", "to": "regulation 881/2002", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 2529/2001", "to": "regulation 467/2001", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 2529/2001", "to": "regulation 145/2003", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "regulation 90/619", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "regulation 79/267", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "regulation 92/96", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "regulation 93/22", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "regulation 1999/468", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 2002/83/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 79/267/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 90/619/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 92/96/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 91/674/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 73/239/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 93/22/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 91/675/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 83/349/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 2001/34/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 84/253/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 78/660/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 85/611/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 2000/12/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 2002/12/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 2000/64/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 2002/13/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 2001/65/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 2001/108/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 2000/28/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 95/26/eec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "directive 1999/468/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 1365/2000", "to": "decision 1999/468/ec", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 186/2004", "to": "regulation 1982/2005", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 186/2004", "to": "regulation 565/2002", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 186/2004", "to": "regulation 228/2004", "weight": 5}, {"arrows": "to", "color": "blue", "from": "regulation 186/2004", "to": "regulation 537/2004", "weight": 5}]);
+
+        // adding nodes and edges to the graph
+        data = {nodes: nodes, edges: edges};
+
+        var options = {
+    "configure": {
+        "enabled": false
+    },
+    "edges": {
+        "color": {
+            "inherit": true
+        },
+        "smooth": {
+            "enabled": false,
+            "type": "continuous"
+        }
+    },
+    "interaction": {
+        "dragNodes": true,
+        "hideEdgesOnDrag": false,
+        "hideNodesOnDrag": false
+    },
+    "physics": {
+        "enabled": true,
+        "stabilization": {
+            "enabled": true,
+            "fit": true,
+            "iterations": 1000,
+            "onlyDynamicEdges": false,
+            "updateInterval": 50
+        }
+    }
+};
+        
+        
+
+        
+
+        network = new vis.Network(container, data, options);
+
+        
+
+
+        
+
+        return network;
+
+    }
+
+    drawGraph();
+
+</script>
+</body>
+</html>
+"""
