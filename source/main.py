af7df2b0afcc82eb2c6e1ae1d0c284f2a2916bcb
Andrew Lai <andrewlaikh1@gmail.com>
2021-04-25 14:37:47 +0800

    fix issue with missing font

--- source/main.py
+++ source/main.py
@@ -12,7 +12,17 @@ print('current location is: ' + os.getcwd())
 
 graph = Graph()
 
-st.title("EU Legal documents visualised")
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:30px !important;
+    font-weight: bold;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown('<p class="big-font">European Union Legislation Visualised</p>', unsafe_allow_html=True)
 st.markdown("This application helps to **visualise EU documents** and the **relevant provisions cited   **.")
 option = st.selectbox('Select one piece of lex to visualise: ', ('', 'Example 1', 'Example 2', 'Example 3'))
 if option:
